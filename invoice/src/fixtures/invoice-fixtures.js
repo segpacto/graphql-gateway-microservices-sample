ea1ca90234059a12b57856332b00fec57b5d325b
Ivan <segpacto@yahoo.es>
2021-11-03 21:46:06 +0100

    feat: adding invoice api

---
+++ invoice/src/fixtures/invoice-fixtures.js
@@ -0,0 +1,26 @@
+module.exports = [
+  {
+    paymentProfileId: 3622860,
+    invoiceNumber: '20',
+    customerId: '2',
+    customerType: 'PRIVATE',
+    amount: 19,
+    invoiceDate: "01-10-2021"
+  },
+  {
+    paymentProfileId: 3622861,
+    invoiceNumber: '30',
+    customerId: '2',
+    customerType: 'PRIVATE',
+    amount: 300,
+    invoiceDate: "10-04-2020"
+  },
+  {
+    paymentProfileId: 3622866,
+    invoiceNumber: '10',
+    customerId: '1',
+    customerType: 'PRIVATE',
+    amount: 250,
+    invoiceDate: "01-07-2019"
+  }
+]
